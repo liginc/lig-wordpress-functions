3ef6b1bca43c710067c370283b819b38bae26c61
RyotaMoriyama <info@ryotamoriyama.com>
2021-03-07 09:41:04 +0900

    Modify rewrite hook

--- functions/extra/99_rewrite_hooks.php
+++ functions/extra/99_rewrite_hooks.php
@@ -1,58 +1,31 @@
 <?php
 /**
- * ここはパーマリンク設定、リライトルールを変更する処理を記載するファイルです。
+ * カスタム投稿タイプのpermalinkをpost_nameからpost_idに変更する
  */
-add_action('init', 'lig_add_post_type_rules');
+//add_action('registered_post_type', 'replace_permalink_from_name_to_id',1);
 
-function lig_add_post_type_rules()
+function replace_permalink_from_name_to_id()
 {
-    //ここからカスタム投稿タイプのパーマリンクをカスタマイズする処理を記載します。
-    // add_action('init', 'myposttype_rewrite');
-    /**
-     * リライトルール設定
-     * カスタム投稿の数だけ書き込む
-     */
-    function myposttype_rewrite()
-    {
-        global $wp_rewrite;
-        //サンプル　カスタム投稿タイプ　columnのリライトルール
-        $queryarg = 'post_type=column&p='; //post_type=にカスタム投稿名を記載する
-        $wp_rewrite->add_rewrite_tag('%column_id%', '([^/]+)', $queryarg); //第一引数にカスタム投稿名を記載する
-        //第一引数にカスタム投稿名を記載する　第二引数は設定したいURL構成を記載。例ではmyposttype_permalinkで定義している「カスタム投稿名_id」で指定の投稿タイプ記事IDを指定しています。
-        $wp_rewrite->add_permastruct('column', '/column/%column_id%', false);
+    // 変更したい投稿タイプを配列にセット
+    $post_types = [
+        ''
+    ];
 
-        /*
-         * 以降、上記サンプルのようにカスタム投稿タイプ毎に設定をしていく
-         * $queryarg = 'post_type=blog&p=';
-         * $wp_rewrite->add_rewrite_tag('%blog_id%', '([^/]+)',$queryarg);
-         * $wp_rewrite->add_permastruct('blog', '/blog/%blog_id%', false);
-         **/
+    if ( empty($post_types) ) return;
+
+    foreach( $post_types as $pt ) {
+        add_action('registered_post_type', function ($post_type) use ($pt) {
+            if ($pt === $post_type) {
+                add_rewrite_tag("%${pt}_id%", '([0-9]+)', "post_type=${pt}&p=");
+                add_permastruct($pt, "${pt}/%${pt}_id%", []);
+            }
+        }, 10);
+
+        add_filter('post_type_link', function ($post_link, $post) use ($pt) {
+            if ($pt === $post->post_type) {
+                $post_link = str_replace("%${pt}_id%", $post->ID, $post_link);
+            }
+            return $post_link;
+        }, 10, 2);
     }
-
-    // add_filter('post_type_link', 'myposttype_permalink', 1, 3);
-
-    /**
-     * カスタム投稿タイプのパーマリンク構造を変更する。
-     *
-     * @param unknown_type $post_link
-     * @param unknown_type $id
-     * @param unknown_type $leavename
-     *
-     * @return unknown
-     */
-    function myposttype_permalink($post_link, $id = 0, $leavename)
-    {
-        global $wp_rewrite;
-        $post = &get_post($id);
-        if (is_wp_error($post)) {
-            return $post;
-        }
-        $newlink = $wp_rewrite->get_extra_permastruct($post->post_type);
-        $newlink = str_replace('%'.$post->post_type.'_id%', $post->ID, $newlink);
-        $newlink = home_url(user_trailingslashit($newlink));
-
-        return $newlink;
-    }
-
-    //ここまでカスタム投稿タイプのパーマリンクをカスタマイズする処理を記載します。
 }
