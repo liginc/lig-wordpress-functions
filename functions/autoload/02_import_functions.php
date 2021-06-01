513aa8ce25a4ae4e18bbfa581cf017cd7fb0d9fc
森山良太 <moriyama_ryota@liginc.co.jp>
2021-06-01 16:40:19 +0900

    Modify import function

--- functions/autoload/02_import_functions.php
+++ functions/autoload/02_import_functions.php
@@ -3,7 +3,7 @@
 /**
  * Import templates
  */
-function import_template($tpl, $vars = array())
+function import_template(string $tpl, array $vars = [])
 {
     $tpl = ltrim($tpl, '/') . '.php';
     $path = locate_template(array($tpl));
@@ -14,7 +14,25 @@ function import_template($tpl, $vars = array())
     include $path;
 }
 
-function import_part($tpl, $vars = array())
+function import_part(string $tpl, array $vars = [])
 {
     import_template('parts/' . ltrim($tpl, '/'), $vars);
+}
+
+
+/**
+ * $whitelistのキーは変数名、値はデフォルト値
+ */
+function import_vars_whitelist(array $vars, array $whitelist = [])
+{
+    $default_whitelist = [
+        'modifier' => '',
+        'additional' => '',
+    ];
+    $whitelist = array_merge($whitelist, $default_whitelist);
+    $rtn = [];
+    foreach ($whitelist as $k => $default_value) {
+        $rtn[$k] = (array_key_exists($k, $vars)) ? $vars[$k] : $whitelist[$k];
+    }
+    return $rtn;
 }
