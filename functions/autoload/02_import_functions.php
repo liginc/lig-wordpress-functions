099107a56d0058b0f54007e6c278a620f5d4ec1f
森山良太 <moriyama_ryota@liginc.co.jp>
2021-06-01 12:35:14 +0900

    Add function

--- functions/autoload/02_import_functions.php
+++ functions/autoload/02_import_functions.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Import templates
  */
 function import_template($tpl, $vars = array())
 {
-    $tpl = ltrim($tpl, '/').'.php';
+    $tpl = ltrim($tpl, '/') . '.php';
     $path = locate_template(array($tpl));
     if (empty($path)) {
         throw new LogicException("Cannot locate the template '$tpl'.");
@@ -15,5 +16,5 @@ function import_template($tpl, $vars = array())
 
 function import_part($tpl, $vars = array())
 {
-    import_template('parts/'.ltrim($tpl, '/'), $vars);
+    import_template('parts/' . ltrim($tpl, '/'), $vars);
 }
